ccbfeb129b6198e755e200e0ec30bd5f029068f2
pierre nikolas contessotto <pierrenikolas@gmail.com>
2024-08-07 17:02:52 +0900

    finished

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  CATEGORIES = ["italian", "chinese", "japanese", "french", "belgian"]
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+
 end
